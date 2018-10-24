19243a57fdedcfee04e5be8c876682ea9c78de28
John Riccardi <riccjohn@gmail.com>
2018-10-24 12:44:14 -0500

    Updates

---
+++ precache-manifest.bf454eed4ff39032690ebe13b40dadcd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e258ea1747dec9816919",
+    "url": "/home/static/js/runtime~main.e258ea17.js"
+  },
+  {
+    "revision": "dc746e64f9315b97a3a9",
+    "url": "/home/static/js/main.dc746e64.chunk.js"
+  },
+  {
+    "revision": "d39ff2b44f9ad764b179",
+    "url": "/home/static/js/1.d39ff2b4.chunk.js"
+  },
+  {
+    "revision": "dc746e64f9315b97a3a9",
+    "url": "/home/static/css/main.e258ba3f.chunk.css"
+  },
+  {
+    "revision": "98acad9d02166f8a3769c9e0ebb0b96b",
+    "url": "/home/index.html"
+  }
+];
