e0bdccf0210d36f4066e52370a51b8aab891ff41
John Riccardi <riccjohn@gmail.com>
2018-10-24 12:25:21 -0500

    Building project

--- src/Contact.js
+++ src/Contact.js
@@ -4,15 +4,7 @@ const Contact = () => {
   return (
     <div id="contact">
       <div className="content">
-        <a
-          href="https://www.behance.net/riccjohn"
-          target="_blank"
-          rel="noopener noreferrer"
-          title="Behance"
-          id="behance"
-        >
-          <img src="/behance-icon.svg" alt="behance" />
-        </a>
+
         <a
           href="https://github.com/riccjohn"
           target="_blank"
@@ -22,15 +14,7 @@ const Contact = () => {
         >
           <img src="/github-icon.svg" alt="github" />
         </a>
-        <a
-          href="https://dribbble.com/jriccardi"
-          target="_blank"
-          rel="noopener noreferrer"
-          title="Dribbble"
-          id="dribbble"
-        >
-          <img src="/dribbble-icon.svg" alt="dribble" />
-        </a>
+        <p>Made with <span>♥</span> in Madison, WI</p>
         <a
           href="mailto:john@jriccardi.com"
           target="_blank"
