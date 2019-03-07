71c9f585ffa65bca35cd362d8121800ce7b2bee0
riccjohn <riccjohn@gmail.com>
2019-03-07 15:02:19 -0600

    Updates

---
+++ precache-manifest.c69aa96c7292d35395f32f19cde91845.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "45cbaa7471d22461f80e",
+    "url": "/static/css/main.3815f358.chunk.css"
+  },
+  {
+    "revision": "45cbaa7471d22461f80e",
+    "url": "/static/js/main.a6f799c6.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "b1167aa97f86ecbd768f",
+    "url": "/static/js/2.51fd3a78.chunk.js"
+  },
+  {
+    "revision": "5841aad20b0133553ab31ed1b0ab68c6",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "www.jriccardi.com/precache-manifest.83dcc97f3749dd0794c70683b63c578a.js"
+  "/precache-manifest.c69aa96c7292d35395f32f19cde91845.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("www.jriccardi.com/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
