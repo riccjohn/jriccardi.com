cdc6fbc14d891915a15ac342d0d002ae36344243
riccjohn <riccjohn@gmail.com>
2019-03-07 14:35:04 -0600

    feat: Restructure app to pull info from data file
    Restructure app to display projects based on an array of data in src/projectData.js so it can be
    updated more easily

---
+++ src/ProjectCards.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import SingleProject from './SingleProject';
+import { codeProjects } from './projectData';
+
+const ProjectCards = props => {
+  return (
+    <div id="work">
+      <h1 className="button">Projects</h1>
+      <div className="content">
+        { codeProjects.map( project => (
+          <SingleProject key={project.id} project={project} />
+          ))
+        }
+      </div>
+    </div>
+  )
+}
+
+export default ProjectCards;
